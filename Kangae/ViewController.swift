4bda7e6b40d54480785e63d79e26fea01beef190
Francis Adewale <francisadewale1992@gmail.com>
2020-10-27 17:19:51 +0000

    add functionality

--- Kangae/ViewController.swift
+++ Kangae/ViewController.swift
@@ -6,22 +6,44 @@
 //
 
 import UIKit
+import NRSpeechToText
+
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var lightbulbImage: UIImageView!
+    
+    var timer: Timer? = nil // Property
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         lightbulbImage.image = UIImage(named: "unlitBulb")
+        self.timer = Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(handleTimer), userInfo: nil, repeats: false)
     }
     
-    
-    override func viewWillAppear(_ animated: Bool) {
-        let timer = Timer(fire: <#T##Date#>, interval: <#T##TimeInterval#>, repeats: <#T##Bool#>, block: <#T##(Timer) -> Void#>)
+    @objc func handleTimer() {
         lightbulbImage.image = UIImage(named: "litBulb")
+        let tap = UITapGestureRecognizer(target: self, action: #selector(stop))
+        lightbulbImage.addGestureRecognizer(tap)
+        lightbulbImage.isUserInteractionEnabled = true
+
+        NRSpeechToText.shared.startRecording {(result: String?, isFinal: Bool, error: Error?) in
+            if error == nil {
+                if let speech = result {
+                    print(speech)
+                }
+            } else {
+                print("Could not retrieve recording")
+            }
+        }
     }
-
-
+    
+    @objc func stop() {
+        if NRSpeechToText.shared.isRunning {
+            NRSpeechToText.shared.stop()
+        }
+    }
+    
 }
 
